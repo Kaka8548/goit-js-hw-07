922071ba03fc428374c792b552c125a982fb0299
Kaka8548 <katyakatya8548@gmail.com>
2022-11-09 22:44:42 +0400

    ДЗ1: створено гелерею зображень з даного об'єкту

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,19 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryDivEl = document.querySelector('.gallery');
+
+const addImgsToGallery = arr => {
+  const imgList = arr
+    .map(el => {
+      const imgEl = `<div class='gallery__item'><a src='#' class='gallery__link'><img class='gallery__image' src='${el.preview}' alt='${el.description}' data-orig-size='${el.original}'></a></div>`;
+
+      return imgEl;
+    })
+    .join('');
+
+  galleryDivEl.insertAdjacentHTML('afterbegin', imgList);
+};
+
+addImgsToGallery(galleryItems);
